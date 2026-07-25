361805e9d2f0e373a36535bfe08ca0abfa563eac
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2026-07-25 18:06:41 +0500

    feat: add views and URL routing

---
+++ resume_website/main/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    HomeView,
+    PortfolioListView,
+    PortfolioDetailView,
+    SkillsView,
+    ContactView,
+)
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('portfolio/', PortfolioListView.as_view(), name='portfolio_list'),
+    path('portfolio/<slug:slug>/', PortfolioDetailView.as_view(), name='portfolio_detail'),
+    path('skills/', SkillsView.as_view(), name='skills'),
+    path('contacts/', ContactView.as_view(), name='contacts'),
+]
--- resume_website/main/views.py
+++ resume_website/main/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
+from .models import Project, Skill, Education
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    """Главная страница"""
+    template_name = 'main/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['education_list'] = Education.objects.all()
+        return context
+
+
+class PortfolioListView(ListView):
+    """Все проекты и кейсы"""
+    model = Project
+    template_name = 'main/portfolio_list.html'
+    context_object_name = 'projects'
+
+
+class PortfolioDetailView(DetailView):
+    """Отдельный проект или кейс"""
+    model = Project
+    template_name = 'main/portfolio_detail.html'
+    context_object_name = 'project'
+
+
+class SkillsView(TemplateView):
+    """Страница навыков"""
+    template_name = 'main/skills.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['skills'] = Skill.objects.all()
+        return context
+
+
+class ContactView(TemplateView):
+    """Контакты"""
+    template_name = 'main/contacts.html'
