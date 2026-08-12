509eda34f2c25534c32e0051b48d4672e44a50b5
VVP04 <vova.plesovkikh@gmail.com>
2026-08-12 22:25:48 +0500

    feat: configure navigation menu

--- main/urls.py
+++ main/urls.py
@@ -1,21 +1,32 @@
 from django.urls import path
 
 from .views import (
+    AboutView,
     ContactView,
     HomeView,
     PortfolioDetailView,
     PortfolioListView,
-    SkillsView,
+    ResumeView,
 )
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('portfolio/', PortfolioListView.as_view(), name='portfolio_list'),
+
+    path('about/', AboutView.as_view(), name='about'),
+
+    path('resume/', ResumeView.as_view(), name='resume'),
+
+    path(
+        'portfolio/',
+        PortfolioListView.as_view(),
+        name='portfolio_list',
+    ),
+
     path(
         'portfolio/<slug:slug>/',
         PortfolioDetailView.as_view(),
         name='portfolio_detail',
     ),
-    path('skills/', SkillsView.as_view(), name='skills'),
+
     path('contacts/', ContactView.as_view(), name='contacts'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.views.generic import DetailView, ListView, TemplateView
 
-from .models import Education, Project, Skill
+from .models import Education, Project
 
 
 class HomeView(TemplateView):
-    """Главная страница"""
+    """Home page."""
+
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
@@ -13,30 +14,35 @@ class HomeView(TemplateView):
         return context
 
 
+class AboutView(TemplateView):
+    """About page."""
+
+    template_name = 'main/about.html'
+
+
+class ResumeView(TemplateView):
+    """Resume page."""
+
+    template_name = 'main/resume.html'
+
+
 class PortfolioListView(ListView):
-    """Все проекты и кейсы"""
+    """All projects and cases."""
+
     model = Project
     template_name = 'main/portfolio_list.html'
     context_object_name = 'projects'
 
 
 class PortfolioDetailView(DetailView):
-    """Отдельный проект или кейс"""
+    """Individual project or case."""
+
     model = Project
     template_name = 'main/portfolio_detail.html'
     context_object_name = 'project'
 
 
-class SkillsView(TemplateView):
-    """Страница навыков"""
-    template_name = 'main/skills.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['skills'] = Skill.objects.all()
-        return context
-
-
 class ContactView(TemplateView):
-    """Контакты"""
+    """Contact page."""
+
     template_name = 'main/contacts.html'
