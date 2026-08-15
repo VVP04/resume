fb80e763afd779d3fc134017dadb825bd75560c9
VVP04 <vova.plesovkikh@gmail.com>
2026-08-15 16:05:41 +0500

    feat: add about page

--- main/views.py
+++ main/views.py
@@ -1,48 +1,37 @@
 from django.views.generic import DetailView, ListView, TemplateView
 
-from .models import Education, Project
+from .models import Project
 
 
 class HomeView(TemplateView):
-    """Home page."""
 
     template_name = 'home.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['education_list'] = Education.objects.all()
-        return context
-
 
 class AboutView(TemplateView):
-    """About page."""
 
-    template_name = 'main/about.html'
+    template_name = 'about.html'
 
 
 class ResumeView(TemplateView):
-    """Resume page."""
 
-    template_name = 'main/resume.html'
+    template_name = 'resume.html'
 
 
 class PortfolioListView(ListView):
-    """All projects and cases."""
 
     model = Project
-    template_name = 'main/portfolio_list.html'
+    template_name = 'portfolio_list.html'
     context_object_name = 'projects'
 
 
 class PortfolioDetailView(DetailView):
-    """Individual project or case."""
 
     model = Project
-    template_name = 'main/portfolio_detail.html'
+    template_name = 'portfolio_detail.html'
     context_object_name = 'project'
 
 
 class ContactView(TemplateView):
-    """Contact page."""
 
-    template_name = 'main/contacts.html'
+    template_name = 'contacts.html'
