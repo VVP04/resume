4881bd40cdbfb56abcbc46f69eca23144d6d1231
VVP04 <vova.plesovkikh@gmail.com>
2026-08-25 20:16:44 +0500

    feat: add portfolio and contacts page stubs

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 
 from .views import (
     AboutView,
-    ContactView,
     HomeView,
-    PortfolioDetailView,
-    PortfolioListView,
     ResumeView,
 )
 
@@ -15,18 +12,4 @@ urlpatterns = [
     path('about/', AboutView.as_view(), name='about'),
 
     path('resume/', ResumeView.as_view(), name='resume'),
-
-    path(
-        'portfolio/',
-        PortfolioListView.as_view(),
-        name='portfolio_list',
-    ),
-
-    path(
-        'portfolio/<slug:slug>/',
-        PortfolioDetailView.as_view(),
-        name='portfolio_detail',
-    ),
-
-    path('contacts/', ContactView.as_view(), name='contacts'),
 ]
